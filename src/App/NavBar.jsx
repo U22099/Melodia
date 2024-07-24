9b21eb2b5a3b0cf32ea12e107cc90e4d58c2b383
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-24 07:19:06 +0100

    Update NavBar.jsx

--- src/App/NavBar.jsx
+++ src/App/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = (props) => {
     const [text, setText] = useState("Save");
     const [errorText, setErrorText] = useState("");
     const [fileCount, setFileCount] = useState(0);
-    const [upload, setUpload] = useState({state: , msg: ""});
+    const [upload, setUpload] = useState({state: null, msg: ""});
     const [confirm, setConfirm] = useState(false);
     const [admin, setAdmin] = useState(false);
     const [showAdminPanel, setShowAdminPanel] = useState(false);
