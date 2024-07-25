4bf556697928ee1b63613c160dc9fbda5e720359
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-25 03:18:14 +0100

    Update NavBar.jsx

--- src/App/NavBar.jsx
+++ src/App/NavBar.jsx
@@ -182,7 +182,7 @@ const NavBar = (props) => {
                         "artist": tag.tags.artist || '---',
                         "title": tag.tags.title,
                         "genre": tag.tags.genre || '---',
-                        "image": (tag.tags.picture ? await toBase64(new Blob([new Uint8Array(tag.tags.picture.data)], {type: tag.tags.picture.format})) : await toBase64("image.JPG"))
+                        "image": (tag.tags.picture ? await toBase64(new Blob([new Uint8Array(tag.tags.picture.data)], {type: tag.tags.picture.format})) : await toBase64(new Blob([atob(image.split(',')[1]), {type: image.split(',')[0]}))
                     }
                     console.log(result);
                     resolve(result);
