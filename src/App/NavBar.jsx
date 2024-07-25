964bb487ed995103fc0032d0dc850f4bf5bf7293
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-25 21:41:15 +0100

    Add files via upload

--- src/App/NavBar.jsx
+++ src/App/NavBar.jsx
@@ -182,7 +182,7 @@ const NavBar = (props) => {
                         "artist": tag.tags.artist || '---',
                         "title": tag.tags.title,
                         "genre": tag.tags.genre || '---',
-                        "image": (tag.tags.picture ? await toBase64(new Blob([new Uint8Array(tag.tags.picture.data)], {type: tag.tags.picture.format})) : image)
+                        "image": (tag.tags.picture ? await toBase64(new Blob([new Uint8Array(tag.tags.picture.data)], {type: tag.tags.picture.format})) : toBase64("image.JPG"))
                     }
                     console.log(result);
                     resolve(result);
