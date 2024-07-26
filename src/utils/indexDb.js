64fd2ca59440b69486de05984935824af554d81b
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-26 13:44:24 +0100

    Update indexDb.js

--- src/utils/indexDb.js
+++ src/utils/indexDb.js
@@ -1,15 +1,18 @@
 const init = async (objStore) => {
+    console.log("init");
     const db = await indexedDB.open("Melodia", 1);
     db.createObjectStore(objStore);
     return db;
 }
 const saveData = async (data, objStore) => {
+    console.log("savedata")
     const db = await init(objStore);
     const transaction = db.transaction(objStore, 'readwrite');
     const store = transaction.objectStore(objStore);
     await store.add(data, 1);
 }
 const getData = async (objStore) => {
+    console.log("called getData");
     const db = await init(objStore);
     const transaction = db.transaction(objStore, 'readonly');
     const store = transaction.objectStore(objStore);
