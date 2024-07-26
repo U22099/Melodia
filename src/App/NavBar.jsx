421208e22f035644e3f332a9354b626d46644e8b
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-26 13:32:25 +0100

    Update NavBar.jsx

--- src/App/NavBar.jsx
+++ src/App/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import AdminPanel from '../AdminPanel'
-import indexDB from '../utils/indexDb.js'
+import * as indexDB from '../utils/indexDb.js'
 import toBase64 from './../utils/Base64.js'
 import ErrorDialog from '../utils/ErrorDialog'
 import SuccessDialog from '../utils/SuccessDialog'
