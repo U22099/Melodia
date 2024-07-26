7023d7291bbe4ef844ad1ee5d4767a24bf53f332
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-26 21:35:11 +0100

    Update Body.jsx

--- src/App/Body.jsx
+++ src/App/Body.jsx
@@ -47,6 +47,7 @@ const Body = (props) => {
             const url = origin.default.origin + '/musicapi';
             const response = await axios.get(url, { withCredentials: true });
             const data = response.data.music.sort((a, b) => a.title.localeCompare(b.title));
+            console.log(data);
             indexedDB.saveData(data, "MusicData");
             setMusic(data);
             console.log(response.data.music);
