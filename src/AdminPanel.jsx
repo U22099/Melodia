8189c2ca088978bb41f7b996c9767aeaa11d2723
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-26 02:27:59 +0100

    Update AdminPanel.jsx

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -22,7 +22,7 @@ function AdminPanel(props) {
                     duration: 0.5,
                 }
             }}
-            className="flex flex-col bg-black rounded-xl p-[20px] dialog md:w-[60vw] w-[95vw] justify-start items-start gap-[10px] h-[80vh] overflow-y-scroll scrollbar py-[10px]">
+            className="flex flex-col bg-black rounded-xl p-[20px] dialog md:w-[60vw] w-[95vw] justify-start items-start gap-[10px] h-[80vh] overflow-y-scroll overflow-x-hidden scrollbar py-[10px]">
             <div className="flex gap-[10px] justify-end p-[5px] pb-[0px] w-[100%]">
                 <MdRefresh className={(spinning ? "animate-spin-once " : "") + "cursor-pointer text-[2.6em] fill-[var(--secondary-color)]"} onClick={refreshState} />
                 <MdOutlineClose className="fill-[var(--secondary-color)] cursor-pointer text-[2.6em]" onClick={() => props.setShowAdminPanel(false)} />
@@ -35,8 +35,8 @@ function AdminPanel(props) {
                             <li key={i}>
                                 <div className="cursor-pointer p-[10px] rounded-[10px] flex gap-[20px] items-center">
                                     <img src={x.image} alt="Music Picture" className="bg-[black] rounded-full w-24 h-24" />
-                                    <div className="max-w-[80%] overflow-wrap">
-                                        <h1 className="text-[1.8em] bold md:text-[2.2em] font-[serif] overflow-wrap max-w-[90%]">{x.username}</h1>
+                                    <div className="max-w-[80%] truncate">
+                                        <h1 className="text-[1.8em] bold md:text-[2.2em] font-[serif] max-w-[250px] truncate md:max-w-[350px]">{x.username}</h1>
                                         <p>Email: {x.email}</p>
                                     </div>
                                 </div>
