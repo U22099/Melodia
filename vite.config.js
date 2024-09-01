b99058ec27bdf9930d92bd4c9f5e88c32444fa88
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-01 05:35:55 +0000

    Adding PWA feature

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import {VitePWA} from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Melodia",
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Melodia MSA',
+        short_name: 'Melodia',
+        description: 'Melodia Music Streaming App',
+        background_color: '#121212', 
+        display: 'standalone', 
+        start_url: '/Melodia',
+        scope: '/',
+        orientation: 'portrait',
+        theme_color: '#121212',
+        icons: [
+		{
+			src: "pwa-64x64.png",
+			sizes: "64x64",
+			type: "image/png"
+		},
+		{
+			src: "pwa-192x192.png",
+			sizes: "192x192",
+			type: "image/png"
+		},
+  		{
+    			src: "pwa-512x512.png",
+    			sizes: "512x512",
+    			type: "image/png"
+  		},
+  		{
+    			src: "maskable-icon-512x512.png",
+    			sizes: "512x512",
+    			type: "image/png",
+    			purpose: "maskable"
+  		}
+	]
+      }
+    })
+  ],
   assetsInclude: ["**/*.{JPG,PNG,jpg,png}"],
   optimizeDeps:{
     override: true
